6c7460ebc84077b7ed037c5746e2e90b75a4274b
Valentin Koychev <114136127+vrkoychev@users.noreply.github.com>
2023-05-08 20:23:59 +0300

    Implemented CarRepository

---
+++ Repositories/CarRepository/CarRepository.cs
@@ -0,0 +1,56 @@
+﻿namespace Repositories.CarRepository
+{
+    using Data;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class CarRepository : ICarRepository
+    {
+        private readonly ApplicationDbContext appDbContext;
+        public CarRepository(ApplicationDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Car> GetAllCars => appDbContext.Cars
+            .Include(x => x.Brand)
+            .Include(x => x.CarExtras).ThenInclude(x => x.Extra)
+            .AsNoTracking().ToList();
+
+        public Car GetCarById(int id) => appDbContext.Cars
+            .Include(x => x.Brand)
+            .Include(x => x.CarExtras).ThenInclude(x => x.Extra)
+            .AsNoTracking().SingleOrDefault(x => x.Id == id);
+
+        public Car GetCarByModel(string model) => appDbContext.Cars
+            .Include(x => x.Brand)
+            .Include(x => x.CarExtras).ThenInclude(x => x.Extra)
+            .AsNoTracking().SingleOrDefault(x => x.Model == model);
+
+        public void Add(Car car)
+        {
+            appDbContext.Cars.Add(car);
+            appDbContext.SaveChanges();
+        }
+
+        public void Update(Car car)
+        {
+            var oldCar = appDbContext.Cars.Single(x => x.Id == car.Id);
+
+            oldCar.Model = car.Model;
+            oldCar.Year = car.Year;
+            oldCar.BrandId = car.BrandId;
+
+            appDbContext.Entry(oldCar).State = EntityState.Modified;
+            appDbContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var car = appDbContext.Cars.Find(id);
+
+            appDbContext.Cars.Remove(car);
+            appDbContext.SaveChanges();
+        }
+    }
+}
---
+++ Repositories/CarRepository/ICarRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Repositories.CarRepository
+{
+    using Models;
+
+    public interface ICarRepository
+    {
+        Car GetCarById(int carId);
+        Car GetCarByModel(string model);
+        IEnumerable<Car> GetAllCars { get; }
+        void Add(Car car);
+        void Update(Car car);
+        void Delete(int id);
+    }
+}
